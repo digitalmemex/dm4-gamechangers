4fdec186124234358926ff8bf21c33a0d2daafc1
Robert Schuster <robert.schuster.rs01@gmail.com>
2017-01-18 11:33:26 +0100

    * depend on DM 4.8.5  * disabled migration 11 (stay with 10)

--- src/main/java/fi/aalto/gamechangers/migrations/Migration11.java
+++ src/main/java/fi/aalto/gamechangers/migrations/Migration11.java
@@ -5,7 +5,6 @@ import static fi.aalto.gamechangers.GamechangersPlugin.NS;
 import java.util.Arrays;
 
 import de.deepamehta.core.TopicType;
-import de.deepamehta.core.model.TopicTypeModel;
 import de.deepamehta.core.service.Migration;
 
 
@@ -15,17 +14,16 @@ public class Migration11 extends Migration {
 
 	@Override
 	public void run() {
-		TopicType tt = dm4.getTopicType(NS("comment"));
-		TopicTypeModel ttm = (TopicTypeModel) tt.getModel();
-		ttm.setLabelConfig(Arrays.asList("dm4.contacts.person_name", "dm4.contacts.email_address"));
-		dm4.updateTopicType(ttm);
+/* CAN ONLY BE ACTIVATED WHEN USING DM 4.8.6 !		
+		dm4.getTopicType(NS("comment"))
+			.setLabelConfig(Arrays.asList("dm4.contacts.person_name", "dm4.contacts.email_address"));
+		TopicType tt = dm4.getTopicType(NS("proposal"));
+		tt.getAssocDef("dm4.datetime.date#dm4.events.from").getChildTopics().set("dm4.core.include_in_label", false);
+		tt.getAssocDef("dm4.datetime.date#dm4.events.to").getChildTopics().set("dm4.core.include_in_label", false);
 		
-		/* TODO: Some bug prevents changes to the proposals
 		tt = dm4.getTopicType(NS("proposal"));
-		ttm = (TopicTypeModel) tt.getModel();
-		ttm.setLabelConfig(Arrays.asList("dm4.contacts.person_name", "dm4.contacts.email_address"));
-		dm4.updateTopicType(ttm);
-		*/
+		tt.setLabelConfig(Arrays.asList("dm4.contacts.person_name", "dm4.contacts.email_address"));
+*/		
 	}
 	
 }
